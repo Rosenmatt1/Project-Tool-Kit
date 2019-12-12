5e3bb25733874937ceabd30081cd2a83c6a2d403
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-12-11 17:05:40 -0700

    Added a styling home pagr button

---
+++ src/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Button.scss'
+
+function Card(props) {
+  return (
+    <div class="container-1">
+      <div class="btn btn-one">
+        <span>Enter Site</span>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/Card/Card.js
@@ -0,0 +1,12 @@
+import React from'react';
+import './Card.scss'
+
+function Card(props) {
+  return (
+    <div>
+      This is the card.
+    </div>
+  )
+}
+
+export default Card;
