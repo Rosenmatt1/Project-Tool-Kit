3398313fb2292afe93451766a1e9051a3530cd5b
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-03-24 11:58:59 -0600

    Deployed updates to firebase

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -7,7 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Purchase from '../Purchase/Purchase.js';
+// import Purchase from '../Purchase/Purchase.js';
 // import Clothing from '../Clothing/Clothing.js';
 import { Route, Link } from "react-router-dom";
 // import { Grid } from '@material-ui/core';
