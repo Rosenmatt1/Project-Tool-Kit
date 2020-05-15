00b0826588551a3cfc36a4370ad336b636da19b1
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-05-14 23:01:00 -0600

    Added a purchase card,but will need to add the Shopify card next

--- src/Purchase/Purchase.js
+++ src/Purchase/Purchase.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 // import { Route, Link } from "react-router-dom";
 
-import Products from '../Products/Products.js'
+// import Products from '../Products/Products.js'
 // import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
@@ -31,52 +31,35 @@ function Purchase(props) {
   console.log("PURCHASE Rendered!!!")
 
   return (
-    <div>
-    <h1 className={classes.purchase}>Purchase</h1>
+ 
 
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    <p>fvfvfvfvfvd</p>
-    </div>
-
-    // <Products />
-
-    // <Card className={classes.card}>
-    // <CardActionArea>
-    //    <CardMedia 
-    //       component="img"
-    //       alt={props.name}
-    //       height="140"
-    //       image="//unsplash.it/250/250"
-    //       title={props.name}
-    //     />
-    //     <CardContent>
-    //       <Typography gutterBottom variant="h5" component="h2">
-    //         {props.name}
-    //       </Typography>
-    //       <Typography className={classes.description} variant="body2" color="textSecondary" component="p">
-    //         {props.description}
-    //       </Typography>
-    //     </CardContent>
-    //   </CardActionArea>
-    //   <CardActions>
+    <Card className={classes.card}>
+    <CardActionArea>
+       <CardMedia 
+          component="img"
+          alt={props.name}
+          height="140"
+          image="//unsplash.it/250/250"
+          title={props.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.name}
+          </Typography>
+          <Typography className={classes.description} variant="body2" color="textSecondary" component="p">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
 
 
-    //     <Button size="small" color="primary">
-    //       Purchase
-    //     </Button>
+        <Button size="small" color="primary">
+          Purchase
+        </Button>
 
-    //   </CardActions>
-    // </Card>
+      </CardActions>
+    </Card>
   );
 }
 
